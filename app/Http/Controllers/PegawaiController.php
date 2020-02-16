87b7c93c6cc5ca121c0ea6c4d122ae9c9b0a8956
zakkyf <zakkyfirdaus@icloud.com>
2020-02-17 02:20:26 +0700

    Pembuatan fungsi dari edit dan hapus dengan menggunakan template sbAdmin

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $data_pegawai = \App\Pegawai::all();
+        if($request->has('cari')){ //fungsi dari form pencarian pada tabel nama_depan saja
+                $data_pegawai = \App\Pegawai::where('nama_depan','LIKE','%'.$request->cari.'%')->get();
+            } else {
+                $data_pegawai = \App\Pegawai::all();
+        }
         return view('pegawai.index',['data_pegawai' => $data_pegawai]);
     }
 
@@ -17,4 +21,25 @@ class PegawaiController extends Controller
         \App\Pegawai::create($request->all());
         return redirect('/pegawai')->with('sukses','Data pegawai berhasil ditambahkan');
     }
+
+    public function edit($id)
+    {
+        $pegawai = \App\Pegawai::find($id);
+        return view('pegawai/edit',['pegawai'=>$pegawai]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $pegawai = \App\Pegawai::find($id);
+        $pegawai->update($request->all());
+        return redirect('/pegawai')->with('sukses','Data telah berhasil diupdate');
+    }
+
+    public function delete($id)
+    {
+        $pegawai = \App\Pegawai::find($id);
+        $pegawai->delete($pegawai);
+        return redirect('/pegawai')->with('sukses', 'Data telah berhasil dihapus');
+    }
+
 }
--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -1,115 +1,185 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>e-Gov</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
 
-    <div class="container">
-        @if(session('sukses'));
-        <div class="alert alert-success" role="alert">
-            {{session('sukses')}}
-        </div>
-        @endif
-        <div class="row">
-            <div class="col-6">
-                <h1>Data Pegawai</h1>
+            @if(session('sukses'))
+            <div class="alert alert-success" role="alert">
+                {{session('sukses')}}
             </div>
-            <div class="col-6">
-                <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
-                        Tambah Data Pegawai
-                    </button>
+            @endif
+
+                <!-- DataTales Example -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h1 class="m-0 font-weight-bold text-primary">Informasi Data Pegawai</h1>
+                        <button type="button" class="btn btn-primary btn-icon-split float-right" data-toggle="modal" data-target="#exampleModal">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-flag"></i>
+                            </span>
+                            <span class="text">Tambah Data Pegawai</span>
+                        </button>
+                    </div>
+
+                        <div class="card-body">
+                            <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>Nama Depan</th>
+                                        <th>Nama Belakang</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Agama</th>
+                                        <th>Alamat</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+
+                                @foreach ($data_pegawai as $pegawai)
+
+                                <tr>
+                                    <td>{{$pegawai->nama_depan}}</td>
+                                    <td>{{$pegawai->nama_belakang}}</td>
+                                    <td>{{$pegawai->jenis_kelamin}}</td>
+                                    <td>{{$pegawai->agama}}</td>
+                                    <td>{{$pegawai->alamat}}</td>
+                                    <td>
+                                        <a href="/pegawai/{{$pegawai->id}}/edit" class="btn btn-outline-warning">Edit</a>
+                                        <a href="/pegawai/{{$pegawai->id}}/delete" onclick="return confirm('Apakah yakin data ini ingin dihapus?')" class="btn btn-outline-danger">Hapus</a>
+                                    </td>
+                                </tr>
+
+                                @endforeach
+                            </table>
+                        </div>
+
+                </div>
+            <!-- Content Row -->
+            <div class="row">
+            <!-- Area Chart -->
+            <div class="col-xl-8 col-lg-7">
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                        <div class="dropdown-header">Dropdown Header:</div>
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="myAreaChart"></canvas>
+                    </div>
+                    </div>
+                </div>
             </div>
-                <table class="table table-hover">
-                    <tr>
-                        <th>Nama Depan</th>
-                        <th>Nama Belakang</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Agama</th>
-                        <th>Alamat</th>
-                    </tr>
 
-                    @foreach ($data_pegawai as $pegawai)
-
-                    <tr>
-                    <td>{{$pegawai->nama_depan}}</td>
-                    <td>{{$pegawai->nama_belakang}}</td>
-                    <td>{{$pegawai->jenis_kelamin}}</td>
-                    <td>{{$pegawai->agama}}</td>
-                    <td>{{$pegawai->alamat}}</td>
-                    </tr>
-
-                    @endforeach
-                </table>
-
-        </div>
-    </div>
-
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data Pegawai</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-            <div class="modal-body">
-                <form action="/pegawai/create" method="POST">
-                    {{csrf_field()}}
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nama Depan</label>
-                        <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <small id="emailHelp" class="form-text text-muted">Kami tidak akan pernah membagikan nama depan Anda dengan orang lain.</small>
+            <!-- Pie Chart -->
+            <div class="col-xl-4 col-lg-5">
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                        <div class="dropdown-header">Dropdown Header:</div>
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nama Belakang</label>
-                        <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <small id="emailHelp" class="form-text text-muted">Kami tidak akan pernah membagikan nama belakang Anda dengan orang lain.</small>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Jenis Kelamin</label>
-                        <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
-                            <option value="Lainnya">...</option>
-                            <option value="L">Laki-Laki</option>
-                            <option value="P">Perempuan</option>
-                        </select>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                    <div class="chart-pie pt-4 pb-2">
+                        <canvas id="myPieChart"></canvas>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Agama</label>
-                        <select name="agama" class="form-control" id="exampleFormControlSelect1">
-                            <option value="Lainnya">...</option>
-                            <option value="Islam">Islam</option>
-                            <option value="Kristen">Kristen</option>
-                            <option value="Hindu">Hindu</option>
-                            <option value="Budha">Budha</option>
-                            <option value="Lainnya">Lainnya</option>
-                        </select>
+                    <div class="mt-4 text-center small">
+                        <span class="mr-2">
+                        <i class="fas fa-circle text-primary"></i> Direct
+                        </span>
+                        <span class="mr-2">
+                        <i class="fas fa-circle text-success"></i> Social
+                        </span>
+                        <span class="mr-2">
+                        <i class="fas fa-circle text-info"></i> Referral
+                        </span>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Alamat</label>
-                        <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Tambah</button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
+
+
+
+
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data Pegawai</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                    <form action="/pegawai/create" method="POST">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nama Depan</label>
+                            <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">Kami tidak akan pernah membagikan nama depan Anda dengan orang lain.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nama Belakang</label>
+                            <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">Kami tidak akan pernah membagikan nama belakang Anda dengan orang lain.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                            <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+                                <option value="Lainnya">...</option>
+                                <option value="L">Laki-Laki</option>
+                                <option value="P">Perempuan</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Agama</label>
+                            <select name="agama" class="form-control" id="exampleFormControlSelect1">
+                                <option value="Lainnya">...</option>
+                                <option value="Islam">Islam</option>
+                                <option value="Kristen">Kristen</option>
+                                <option value="Hindu">Hindu</option>
+                                <option value="Budha">Budha</option>
+                                <option value="Lainnya">Lainnya</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Alamat</label>
+                            <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Tambah</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            </div>
         </div>
-    </div>
 
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
